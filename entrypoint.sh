53ff106a3c26a3c6568c495a69616c2dd031b363
joeydice <117003062+joeydice@users.noreply.github.com>
2022-10-30 05:32:18 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ProxySite="mirror.umd.edu"
+Vless_UUID="98382fd1-daa8-4300-a2ab-49770765dcb6"
+Vless_Path="/s233"
+Vmess_UUID="98382fd1-daa8-4300-a2ab-49770765dcb6"
+Vmess_Path="/s244"
+Share_Path="/share233"
+
+mkdir /xraybin
+cd /xraybin
+wget -qO- https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | busybox unzip - 
+chmod +x /xray
+
+cd /wwwroot
+tar xvf wwwroot.tar.gz
+rm -rf wwwroot.tar.gz
+
+
+sed -e "/^#/d"\
+    -e "s/\${Vless_UUID}/${Vless_UUID}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s/\${Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    /conf/Xray.template.json >  /xraybin/config.json
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+fi
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    -e "s|\${Share_Path}|${Share_Path}|g"\
+    -e "$s"\
+    /conf/nginx.template.conf > /etc/nginx/conf.d/ray.conf
+
+[ ! -d /wwwroot/${Share_Path} ] && mkdir -p /wwwroot/${Share_Path}
+sed -e "/^#/d"\
+    -e "s|\${_Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${_Vmess_Path}|${Vmess_Path}|g"\
+    -e "s/\${_Vless_UUID}/${Vless_UUID}/g"\
+    -e "s/\${_Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "$s"\
+    /conf/share.html > /wwwroot/${Share_Path}/index.html
+
+cd /xraybin
+./xray run -c ./config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
